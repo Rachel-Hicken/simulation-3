15ad79eb6bdf38917fd3c931a4820d1f81c284e2
Rachel Hicken <rachkh22@gmail.com>
2018-05-18 12:19:57 -0600

    working on redux

--- helo/src/component/Nav/Nav.js
+++ helo/src/component/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {update_username, update_id, update_img} from '../../ducks/reducer';
 
 
-export default function Nav(){
+ function Nav(){
     return(
         <div>
             <Link to='/dashboard'><button>Home</button></Link>
@@ -13,3 +14,13 @@ export default function Nav(){
     )
 }
 
+function mapStateToProps(state){
+    const {username, id, img } = state;
+    return{
+        username,
+        id,
+        img
+    }
+}
+
+export default connect(mapStateToProps, {update_username, update_id, update_img})(Nav);
--- helo/src/ducks/reducer.js
+++ helo/src/ducks/reducer.js
@@ -5,10 +5,39 @@ const initial_state = {
 }
 
 
+UPDATE_USERNAME = 'UPDATE_USERNAME';
+UPDATE_ID = 'UPDATE_ID';
+UPDATE_IMG = 'UPDATE_IMG';
 
+export function update_username(username) {
+    return {
+        type: UPDATE_USERNAME,
+        payload: username
+    }
+}
+
+export function update_id(id) {
+    return {
+        type: UPDATE_ID,
+        payload: id
+    }
+}
+
+export function update_img(img) {
+    return {
+        type: UPDATE_IMG,
+        payload: img
+    }
+}
 
 export default function reducer(state = initial_state, action) {
     switch (action.type) {
+        case UPDATE_USERNAME:
+        return Object.assign({}, state, {username: action.payload});
+        case UPDATE_ID:
+        return Object.assign({}, state, {id: action.payload});
+        case UPDATE_IMG:
+        return Object.assign({}, state, {img: action.payload});
         default:
             return state;
     }
