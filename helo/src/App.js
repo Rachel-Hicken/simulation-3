a04d72e34ddf2477e57d0d9e5907c55777205f9b
Rachel Hicken <rachkh22@gmail.com>
2018-05-18 10:20:39 -0600

    step 1, need to fix conditional rendering

--- helo/src/App.js
+++ helo/src/App.js
@@ -5,15 +5,13 @@ import Dashboard from './component/Dashboard/Dashboard';
 import Form from './component/Form/Form';
 import Post from './component/Post/Post';
 import Nav from './component/Nav/Nav';
+import routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Auth/>
-        <Dashboard/>
-        <Form/>
-        <Post/>
+        {routes}
         <Nav/>
       </div>
     );
--- helo/src/component/Nav/Nav.js
+++ helo/src/component/Nav/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 export default function Nav(){
     return(
-        <div>Nav</div>
+        <div>
+            <Link to='/dashboard'><button>Home</button></Link>
+            <Link to='/new'><button>Post</button></Link>
+            <Link to='/'><button>Logout</button></Link>
+        </div>
     )
 }
