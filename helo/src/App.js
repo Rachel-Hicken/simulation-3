097b732397441b81f0c4d7858fc7b8c77854b165
Rachel Hicken <rachkh22@gmail.com>
2018-05-18 09:52:03 -0600

    initial setup for react

---
+++ helo/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Auth from './component/Auth/Auth';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Post from './component/Post/Post';
+import Nav from './component/Nav/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Auth/>
+        <Dashboard/>
+        <Form/>
+        <Post/>
+        <Nav/>
+      </div>
+    );
+  }
+}
+
+export default App;
